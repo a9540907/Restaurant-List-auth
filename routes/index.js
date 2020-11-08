f65cbdaefe3451e48ec9ff92a844ba6374998905
stan_wang <a7569209@gmail.com>
2020-11-08 13:58:04 +0800

    add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,12 @@ const router = express.Router()
 const home = require('./modules/home')
 const Restaurant = require('./modules/restaurants')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', Restaurant)
 router.use('/sort', sort)
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router

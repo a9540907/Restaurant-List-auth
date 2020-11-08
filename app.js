bf48ce30f5a986ce798886fd1d6a57fa299be118
stan_wang <a7569209@gmail.com>
2020-11-08 14:38:28 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
+const session = require('express-session')
+const usePassport = require('./config/passport')
 
 // 在這裡引用路由器
 const routes = require('./routes')
@@ -14,9 +15,18 @@ const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
 app.use(routes)
 
 app.listen(3000, () => {
--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,12 @@ const Restaurant = require('./modules/restaurants')
 const sort = require('./modules/sort')
 const users = require('./modules/users')
 
+
 router.use('/', home)
+router.use('/users', users)
 router.use('/restaurants', Restaurant)
 router.use('/sort', sort)
-router.use('/users', users)
+
 
 // 匯出路由器
 module.exports = router
